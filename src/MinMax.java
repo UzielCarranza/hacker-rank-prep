e03077ab7b5e2237488384ffc18ec1194dec7760
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-22 21:39:42 -0500

    printing to the console

---
+++ src/MinMax.java
@@ -0,0 +1,11 @@
+import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
+
+public class MinMax {
+    public static void main(String[] args) {
+
+        public static void miniMaxSum(List<Integer> arr) {
+        }
+    }
+}
+
+
